0fa1686290a4ca05578bf5ccf1b2f8a57fbaa453
Lucas Olmedo <lcsolmedosilva@gmail.com>
2017-11-25 15:28:02 -0200

    first tests/first view with Vue

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,7 @@
+<meta name="csrf-token" content="{{csrf_token()}}">
+<div id="app">
+    <example-component>
+        Carregando...
+    </example-component>
+</div>
+<script src="/js/app.js"></script>
